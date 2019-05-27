3f5876df401eb82ffbc0e5eb6d5ce0d2e14b763c
anilnagori <anilnagori@hotmail.com>
2019-05-27 09:56:24 -0700

    Create 3.Friendship Service.cpp

---
+++ 3.Friendship Service.cpp
@@ -0,0 +1,17 @@
+/*
+
+3.Friendship Service
+
+Support follow & unfollow, getFollowers, getFollowings.
+
+Example:
+follow(1, 3)
+getFollowers(1) // return [3]
+getFollowings(3) // return [1]
+follow(2, 3)
+getFollowings(3) // return [1,2]
+unfollow(1, 3)
+getFollowings(3) // return [2]
+
+*/
+
