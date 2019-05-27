bd41fd8e6d08d771987d0aa9e652a3bd1f07e83f
Anil Nagori <anilnagori@hotmail.com>
2019-05-27 13:11:25 -0700

    src code

--- 5.Heart Beat.cpp
+++ 5.Heart Beat.cpp
@@ -7,10 +7,10 @@ server he is alive. If a master server didn't receive any ping request from a sl
 seconds, the master will trigger an alarm (for example send an email) to administrator.
 
 Let's mock the master server, you need to implement the following three methods:
-    initialize(slaves_ip_list, k). salves_ip_list is a list of slaves' ip addresses. k is define above.
+    initialize(slavesIpList, k). salves_ip_list is a list of slaves' ip addresses. k is define above.
     
-    ping(timestamp, slave_ip). This method will be called every time master received a ping request from 
-    one of the slave server. timestamp is the current timestamp in seconds. slave_ip is the ip address of 
+    ping(timestamp, slaveIp). This method will be called every time master received a ping request from 
+    one of the slave server. timestamp is the current timestamp in seconds. slaveIp is the ip address of 
     the slave server who pinged master.
     
     getDiedSlaves(timestamp). This method will be called periodically (it's not guaranteed how long between 
@@ -35,3 +35,85 @@ getDiedSlaves(42)
 >> ["10.173.0.2"]
 
 */
+
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <cassert>
+
+using namespace std;
+
+class HeartBeat {
+public:
+    HeartBeat() : interval(0) { }
+
+    // @param slavesIpList a list of slaves'ip addresses
+    // @param k an integer
+    // @return void
+    void initialize(vector<string> slavesIpList, int k) {
+        for (string ip : slavesIpList) {
+            slaves[ip] = 0;
+        }
+
+        interval = k;
+    }
+
+    // @param timestamp current timestamp in seconds
+    // @param slaveIp the ip address of the slave server
+    // @return nothing
+    void ping(int timestamp, string slaveIp) {
+        if (!slaves.count(slaveIp)) {
+            return;
+        }
+        
+        slaves[slaveIp] = timestamp;
+    }
+
+    // @param timestamp current timestamp in seconds
+    // @return a list of slaves'ip addresses that died
+    vector<string> getDiedSlaves(int timestamp) {
+        vector<string> ret;
+        
+        for (auto& entry : slaves) {
+            if (timestamp - entry.second >= 2 * interval) {
+                ret.push_back(entry.first);
+            }
+        }
+
+        return ret;
+    }
+
+private:
+    unordered_map<string, int> slaves;
+    int interval;
+};
+
+void printSlaves(vector<string>& s) {
+	copy(s.begin(), s.end(), ostream_iterator<string> (cout, " "));
+	cout << endl;
+}
+
+int main() {
+	HeartBeat heatBest;
+
+	heatBest.initialize({"10.173.0.2", "10.173.0.3"}, 10);
+	heatBest.ping(1, "10.173.0.2");
+	
+	auto slaves = heatBest.getDiedSlaves(20);
+
+	printSlaves(slaves);
+	slaves = heatBest.getDiedSlaves(21);
+	printSlaves(slaves);
+	heatBest.ping(22, "10.173.0.2");
+	heatBest.ping(23, "10.173.0.3");
+	heatBest.getDiedSlaves(24);
+	printSlaves(slaves);
+	heatBest.getDiedSlaves(42);
+	printSlaves(slaves);
+
+}
