49aeb580129a77175d25666f3694f5dd1605d678
Anil Nagori <anilnagori@hotmail.com>
2019-05-27 17:02:05 -0700

    src code

--- 8.Memcache.cpp
+++ 8.Memcache.cpp
@@ -2,16 +2,16 @@
 
 8.Memcache 
 
-Impplement memchane which support the following features:
+Implement memcache which support the following features:
 
-- get(curtTime, key). Get the key's value, return 2147483647 if key does not exist.
-- set(curtTime, key, value, ttl). Set the key-value pair in memcache with a time to live (ttl). 
-  The key will be valid from curtTime to curtTime + ttl - 1 and it will be expired after ttl 
+- get(currTime, key). Get the key's value, return 2147483647 if key does not exist.
+- set(currTime, key, value, ttl). Set the key-value pair in memcache with a time to live (ttl). 
+  The key will be valid from currTime to currTime + ttl - 1 and it will be expired after ttl 
   seconds. if ttl is 0, the key lives forever until out of memory.
-- delete(curtTime, key). Delete the key.
-- incr(curtTime, key, delta). Increase the key's value by delta return the new value. Return 
+- remove(currTime, key). Delete the key.
+- incr(currTime, key, delta). Increase the key's value by delta return the new value. Return 
   2147483647 if key does not exist.
-- decr(curtTime, key, delta). Decrease the key's value by delta return the new value. Return 
+- decr(currTime, key, delta). Decrease the key's value by delta return the new value. Return 
   2147483647 if key does not exist.
 
 It's guaranteed that the input is given with increasing currTime.
@@ -50,3 +50,173 @@ incr(12, 1, 1)
 
 */
 
+/*
+  Implement LRU cache with the following additional features:
+  1. Evict expired data 
+  2. Api to increase and decrease data for a given key
+  3. Delete data explictly
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream> 
+#include <cassert>
+#include <climits>
+
+using namespace std;
+
+class Memcache {
+public:
+  Memcache(int capacity) : capacity(capacity) { }
+
+
+  // Get value for key
+  int get(int currTime, int key) {
+    // Return max value if node is not found
+    if (!lookup.count(key)) {
+      return INT_MAX;
+    }
+
+    // Get list node
+    auto it = lookup[key];
+
+    // Remove if expired
+    if (currTime >= it->validTime) {
+      remove(it);
+      return INT_MAX;
+    }
+
+    // Move node to the beginning of linked list
+    // No need to update lookup as node is spliced
+    orderList.splice(orderList.begin(), orderList, it);
+
+    return it->value;
+  }
+
+  // Set value for key
+  void set(int currTime, int key, int value, int timeToLive) {
+    if (timeToLive == 0) {
+      timeToLive = INT_MAX;
+    }
+
+    auto it = lookup.find(key);
+
+    if (it != lookup.end()) {
+      // Already exists so update value and life time
+      it->second->value = value;
+      it->second->validTime = currTime + timeToLive;
+
+      // Move to the front of queue
+      orderList.splice(orderList.begin(), orderList, it->second);
+    } else {
+      // Exceeds capacity so remove LRU node
+      if (lookup.size() == capacity) {
+        remove(prev(orderList.end()));
+      }
+
+      // Add a new node
+      Node node(key, value, timeToLive == INT_MAX ? timeToLive : currTime + timeToLive);
+
+      orderList.insert(orderList.begin(), node);
+      lookup[key] = orderList.begin();
+    }
+  }
+
+  // Remove key
+  void remove(int currTime, int key) {
+    auto it = lookup.find(key);
+
+    if (it == lookup.end()) {
+      return;
+    }
+
+    remove(it->second);
+  }
+
+  // Increment key value
+  int incr(int currTime, int key, int value) {
+    return update(currTime, key, value);
+  }
+
+  // Decrement key value
+  int decr(int currTime, int key, int value) {
+    return update(currTime, key, -value);
+  }
+
+private:
+  // List node
+  struct Node {
+    Node(int key, int value, int validTime) : key(key), value(value), validTime(validTime) { }
+    int key;
+    int value;
+    int validTime;
+  };
+
+  typedef list<Node>::iterator NodeListIter;
+
+  // Update value
+  int update(int currTime, int key, int value) {
+    auto it = lookup.find(key);
+
+    if (it == lookup.end()) {
+      return INT_MAX;
+    }
+
+    if (currTime >= it->second->validTime) {
+      remove(it->second);
+      return INT_MAX;
+    }
+
+    it->second->value += value;
+    orderList.splice(orderList.begin(), orderList, it->second);
+
+    return it->second->value;
+  }
+
+  // Remove node
+  void remove(NodeListIter it) {
+    lookup.erase(it->key);
+    orderList.erase(it);
+  }
+
+  size_t capacity;
+  list<Node> orderList;
+  unordered_map<int, NodeListIter> lookup; 
+};
+
+int main() {
+  cout << "test cache 1" << endl;
+
+  Memcache memcache(10);
+
+  cout << memcache.get(1, 0) << endl;
+  memcache.set(2, 1, 1, 2);
+  cout << memcache.get(3, 1) << endl;
+  cout << memcache.get(4, 1) << endl;
+  cout << memcache.incr(5, 1, 1) << endl;
+  memcache.set(6, 1, 3, 0);
+  cout << memcache.incr(7, 1, 1) << endl;
+  cout << memcache.decr(8, 1, 1) << endl;
+  cout << memcache.get(9, 1) << endl;
+  memcache.remove(10, 1);
+  cout << memcache.get(11, 1) << endl;
+  cout << memcache.incr(12, 1, 1) << endl;
+  cout << memcache.get(100, 1) << endl;
+
+  cout << "test cache 2" << endl;
+
+  Memcache memcache2(1);
+  cout << memcache2.get(1, 0) << endl;
+  memcache2.set(2, 1, 3, 6);
+  cout << memcache2.get(3, 1) << endl;
+  memcache2.set(4, 2, 4, 2);
+  cout << memcache2.get(5, 1) << endl;
+  cout << memcache2.get(5, 2) << endl;
+}
